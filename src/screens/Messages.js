8b8cd6b3ffdcb455bf40e7e2c36860bdfd8b6ec4
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2018-04-12 16:41:58 +0200

    Implement sending messages with optimistic response (not waiting for server).

--- src/screens/Messages.js
+++ src/screens/Messages.js
@@ -14,6 +14,7 @@ import MessageInput from "./components/MessageInput";
 
 import { graphql, compose } from "react-apollo";
 import GROUP_QUERY from "../graphql/group.query";
+import CREATE_MESSAGE_MUTATION from "../graphql/create-message.mutation";
 
 const styles = StyleSheet.create({
   container: {
@@ -68,7 +69,11 @@ class Messages extends Component {
 
   send(text) {
     // TODO: send the message
-    console.log(`sending message: ${text}`);
+    this.props.createMessage({
+      groupId: this.props.navigation.state.params.groupId,
+      userId: 1, // faking the user for now
+      text
+    });
   }
 
   keyExtractor = item => item.id.toString();
@@ -113,6 +118,14 @@ class Messages extends Component {
 }
 
 Messages.propTypes = {
+  createMessage: PropTypes.func,
+  navigation: PropTypes.shape({
+    state: PropTypes.shape({
+      params: PropTypes.shape({
+        groupId: PropTypes.number
+      })
+    })
+  }),
   group: PropTypes.shape({
     messages: PropTypes.array,
     users: PropTypes.array
@@ -130,4 +143,51 @@ const groupQuery = graphql(GROUP_QUERY, {
     group
   })
 });
-export default compose(groupQuery)(Messages);
+
+const createMessageMutation = graphql(CREATE_MESSAGE_MUTATION, {
+  props: ({ mutate }) => ({
+    createMessage: ({ text, userId, groupId }) =>
+      mutate({
+        variables: { text, userId, groupId },
+        optimisticResponse: {
+          __typename: "Mutation",
+          createMessage: {
+            __typename: "Message",
+            id: -1, // don't know id yet, but it doesn't matter
+            text, // we know what the text will be
+            createdAt: new Date().toISOString(), // the time is now!
+            from: {
+              __typename: "User",
+              id: 1, // still faking the user
+              username: "Justyn.Kautzer" // still faking the user
+            },
+            to: {
+              __typename: "Group",
+              id: groupId
+            }
+          }
+        },
+        update: (store, { data: { createMessage } }) => {
+          // Read the data from our cache for this query.
+          const groupData = store.readQuery({
+            query: GROUP_QUERY,
+            variables: {
+              groupId
+            }
+          });
+          // Add our message from the mutation to the end.
+          groupData.group.messages.unshift(createMessage);
+          // Write our data back to the cache.
+          store.writeQuery({
+            query: GROUP_QUERY,
+            variables: {
+              groupId
+            },
+            data: groupData
+          });
+        }
+      })
+  })
+});
+
+export default compose(groupQuery, createMessageMutation)(Messages);
